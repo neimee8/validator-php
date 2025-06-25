12bac6cecea0f0ae3001fa22330f1c9d0458e443
neimee8 <bogdans.kologrivovs@gmail.com>
2025-06-25 03:51:22 +0300

    Added schema caching and variable resolving in SchemaManager

--- src/SchemaManager.php
+++ src/SchemaManager.php
@@ -10,9 +10,19 @@ use Neimee8\ValidatorPhp\Exceptions\ValidationSchemaFileException;
 class SchemaManager {
     use StaticConfig;
 
-    private static function getDataFromSchemaFile(string $schema, bool $assoc = true): ?array {
+    private static array $schema_cache = [
+        'resolved' => [],
+        'unresolved' => []
+    ];
+
+    private static function getDataFromSchemaFile(
+        string $schema,
+        bool $assoc = true,
+        $resolve_variables = true
+    ): array {
         self::initConfig();
 
+        // checks if there is requested schema
         $allowed_schemas = [];
 
         foreach (new FilesystemIterator(__DIR__ . '/../' . self::$cnf -> SCHEMAS_DIR) as $file) {
@@ -21,13 +31,107 @@ class SchemaManager {
             }
         }
 
-        if (!in_array($schema, $allowed_schemas, strict: true)) {
-            throw new ValidationSchemaFileException(schema_file: $schema);
+        $loadSchema = function (string $schema_name) use ($allowed_schemas, $assoc): array {
+            if (isset(self::$schema_cache['unresolved'][$schema_name])) {
+                return self::$schema_cache['unresolved'][$schema_name];
+            }
+
+            // if schema does not exists
+            if (!in_array($schema_name, $allowed_schemas, strict: true)) {
+                throw new ValidationSchemaFileException(schema_file: $schema_name);
+            }
+
+            $data = json_decode(
+                file_get_contents(
+                    __DIR__ . '/../' . self::$cnf -> SCHEMAS_DIR . $schema_name . '_schema.json'
+                ),
+                $assoc
+            );
+
+            self::$schema_cache['unresolved'][$schema_name] = $data;
+
+            return $data;
+        };
+
+        $data = $loadSchema($schema);
+
+        if (!$resolve_variables) {
+            return $data;
         }
 
-        $path = __DIR__ . '/../' . self::$cnf -> SCHEMAS_DIR . $schema . '_schema.json';
+        if (isset(self::$schema_cache['resolved'][$schema])) {
+            return self::$schema_cache['resolved'][$schema];
+        }
 
-        return json_decode(file_get_contents($path), $assoc);
+        // resolving variables
+        $resolveVariable = function (string $variable) use ($loadSchema): mixed {
+            $data_source = [
+                'type' => null,
+                'identifier' => null
+            ];
+
+            $keys = [];
+
+            $exploded = explode('>', $variable);
+
+            $given_source = explode(':', $exploded[0]);
+            $data_source['type'] = $given_source[0];
+
+            if (count($given_source) > 1) {
+                $data_source['identifier'] = $given_source[1];
+            }
+
+            if (count($exploded) > 1) {
+                $keys = explode('.', $exploded[1]);
+            }
+
+            if ($data_source['type'] === 'schema' && $data_source['identifier'] !== null) {
+                $data = $loadSchema($data_source['identifier']);
+
+                foreach ($keys as $key) {
+                    if (is_array($data) && array_key_exists($key, $data)) {
+                        $data = $data[$key];
+                    } else {
+                        return "%$variable% [NOT VALID]";
+                    }
+                }
+
+                return $data;
+            }
+
+            return "%$variable% [NOT VALID]";
+        };
+
+        $recursiveWalker = function (mixed &$schema_segment) use (&$recursiveWalker, $resolveVariable): void {
+            foreach ($schema_segment as &$value) {
+                if (is_array($value)) {
+                    $recursiveWalker($value);
+                } elseif (is_string($value)) {
+                    $value = preg_replace_callback(
+                        pattern: '/%(.+?)%/',
+                        callback: function ($matches) use ($resolveVariable): string {
+                            $resolved = $resolveVariable($matches[1]);
+
+                            if (is_scalar($resolved)) {
+                                return (string) $resolved;
+                            }
+
+                            return json_encode($resolveVariable($matches[1]));
+                        },
+                        subject: $value
+                    );
+
+                    $decoded = json_decode($value, true);
+                    $value = json_last_error() === JSON_ERROR_NONE ? $decoded : $value;
+                }
+            }
+        };
+
+        $recursiveWalker($data);
+
+        self::$schema_cache['resolved'][$schema] = $data;
+
+        return $data;
     }
 
     public static function getRuleSchema(): array {
@@ -62,7 +166,7 @@ class SchemaManager {
         return $rule_param_format;
     }
 
-    public static function getRuleParamFormatByGroup(string $rule_group): ?array {
+    public static function getRuleParamFormatByGroup(string $rule_group): array {
         $schema = self::getDataFromSchemaFile('validation_rules');
         $rule_param_format = [];
 
@@ -128,7 +232,7 @@ class SchemaManager {
         return $groups;
     }
 
-    public static function getTypesOfGroup(string $rule_group, bool $nullable = true): ?array {
+    public static function getTypesOfGroup(string $rule_group, bool $nullable = true): array {
         $schema = self::getDataFromSchemaFile('data_types');
         $types = $schema['by_group'][$rule_group] ?? null;
 
